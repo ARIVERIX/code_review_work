c7fe4013ebd6b253a59128099b91284c04acf07c
GuslyakovDaniil <g_daniil2014@inbox.ru>
2023-12-20 09:05:51 +0300

    guslyakovdv

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,26 +6,46 @@ class CalculatorTest {
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        Calculator calculator = new Calculator();
+        int result = calculator.add(3, 4);
+        assertEquals(7, result);
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        Calculator calculator = new Calculator();
+        int result = calculator.dif(8, 3);
+        assertEquals(5, result);
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Calculator calculator = new Calculator();
+        int result = calculator.div(10, 2);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void divByZero() {
+        Calculator calculator = new Calculator();
+        assertThrows(IllegalArgumentException.class, () -> calculator.div(5, 0));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Calculator calculator = new Calculator();
+        int result = calculator.times(6, 7);
+        assertEquals(42, result);
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Calculator calculator = new Calculator();
+        assertEquals(7, calculator.solver(3, 4, "add"));
+        assertEquals(5, calculator.solver(8, 3, "dif"));
+        assertEquals(5, calculator.solver(10, 2, "div"));
+        assertEquals(42, calculator.solver(6, 7, "times"));
+
+        assertThrows(IllegalArgumentException.class, () -> calculator.solver(1, 2, "неподдерживаемый"));
     }
 }
