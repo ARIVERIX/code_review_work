bc8132877c55f5494a430603b1bad7ad824fd49b
Denis <chernov-denis21@mail.ru>
2023-12-20 09:01:41 +0300

    Edit file Calculator, Test and README.md

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,36 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public int add(int a, int b) {
+        return a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+
+    public int dif(int a, int b) {
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+
+    public int div(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+
+    public int times(int a, int b) {
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public int solver(int a, int b, String operation) {
+        switch (operation) {
+            case "add":
+                return add(a, b);
+            case "dif":
+                return dif(a, b);
+            case "div":
+                return div(a, b);
+            case "times":
+                return times(a, b);
+            default:
+                throw new IllegalArgumentException("Неподдерживаемая операция: " + operation);
+        }
     }
 }
